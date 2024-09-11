03d16a4bc3cd619def8e068085b49a6b9360c0c4
Vlad Krendelev <vkren@vkren-mbp22.local>
2024-09-11 16:11:35 -0500

    NDA phqa view

--- ctau_nda_views/6.cde_phq9_nda_view.sql
+++ ctau_nda_views/6.cde_phq9_nda_view.sql
@@ -1,38 +1,71 @@
----- Creating NDA cde_phqa_view
+----------- Create NDA cde_phqa9_view
 
-/*
-Step 1: Create a helper view based on rcap_ctau_scheduling_form.
-
-The definition of rcap_ctau_scheduling_form_agg_view was moved 
-to 0.multiple_purpose_utils_ctau.sql file since it is employed by other NDA views.
-
-create view rcap_ctau_scheduling_form_agg_view 
-as 
-select 
-	sch1.source_subject_id
-	,sch1.event_name
-	, case
-		when sch1.event_name like 'baseline%' then sch1.sched_base_complete_date
-		when sch1.event_name like 'one_month%' then sch2.sched_1mo_complete_date
-		when sch1.event_name like 'six_month%' then sch2.sched_6mo_complete_date
-		when sch1.event_name like 'one_year%' then sch2.sched_1yr_date
-		when sch1.event_name like '18_month%' then sch2.sched_18mo_complete_date
-		when sch1.event_name like '24_month%' then sch2.sched_2yr_complete_date
+select
+    sa2.source_subject_id as subjectkey
+    ,sa2.subject_id as src_subject_id
+	,phqa.source_subject_id -- only for validation; DELETE before submission
+	,phqa.event_name -- only for validation; DELETE before submission
+	,case
+		when phqa.event_name like 'baseline%' then to_char(sched_main.sched_base_complete_date, 'mm/dd/yyyy')
+		when phqa.event_name like 'one_month%' then to_char(sched_main.sched_1mo_complete_date, 'mm/dd/yyyy')
+		when phqa.event_name like 'six_month%' then to_char(sched_main.sched_6mo_complete_date, 'mm/dd/yyyy')
+		when phqa.event_name like 'one_year%' then to_char(sched_main.sched_1yr_complete_date, 'mm/dd/yyyy')
+		when phqa.event_name like '18_month%' then to_char(sched_main.sched_18mo_complete_date, 'mm/dd/yyyy')
+		when phqa.event_name like '24_month%' then to_char(sched_main.sched_2yr_complete_date, 'mm/dd/yyyy')
 	end as interview_date
-	,sch1.sched_base_complete_date
-	,sch1.sched_1mo_complete_date
-	,sch1.sched_6mo_complete_date
-	,sch1.sched_1yr_date
-	,sch1.sched_18mo_complete_date
-	,sch1.sched_2yr_complete_date
-from rcap_ctau_scheduling_form sch1
-inner join rcap_ctau_scheduling_form sch2
-	on sch2.source_subject_id = sch1.source_subject_id
-	and sch2.event_name like 'baseline%'
-order by sch1.source_subject_id, sch1.event_name;
+	,case 
+		when phqa.event_name like 'baseline%' then nda_months_between(sched_main.sched_base_complete_date, dem.dem_ch_dob)
+		when phqa.event_name like 'one_month%' then nda_months_between(sched_main.sched_1mo_complete_date, dem.dem_ch_dob)
+		when phqa.event_name like 'six_month%' then nda_months_between(sched_main.sched_6mo_complete_date, dem.dem_ch_dob)
+		when phqa.event_name like 'one_year%' then nda_months_between(sched_main.sched_1yr_complete_date, dem.dem_ch_dob)
+		when phqa.event_name like '18_month%' then nda_months_between(sched_main.sched_18mo_complete_date, dem.dem_ch_dob)
+		when phqa.event_name like '24_month%' then nda_months_between(sched_main.sched_2yr_complete_date, dem.dem_ch_dob)
+	end as interview_age
+    ,case 
+        when pfhc.hc_sex_birth_cert='1' then 'F'
+        when pfhc.hc_sex_birth_cert='2' then 'M'
+        else null
+    end as sex
+	,phqa.phq_2_val as phq9_1
+	,phqa.phq_1_val as phq9_2
+	,phqa.phq_3_val as phq9_3
+	,phqa.phq_4_val as phq9_4
+	,phqa.phq_5_val as phq9_5
+	,phqa.phq_6_val as phq9_6
+	,phqa.phq_7_val as phq9_7	
+	,phqa.phq_8_val as phq9_8
+	,phqa.phq_9_val as phq9_9
+	,case 
+		when phqa.language = 'sp' then '70' -- Spanish for the United States
+		else '1' -- English
+	end as phqa9_10
+from rcap_phqa phqa -- Attention! rcap_phqa is not a CTAU table.
+inner join rcap_ctau_scheduling_form sched -- to keep CTAU participants only
+	on sched.sched_ctrn_id = phqa.source_subject_id
+	and sched.event_name like 'baseline%'
+inner join subject_alias sa1
+    on sa1.source_subject_id = phqa.source_subject_id
+    and sa1.project_id = 696
+    and sa1.id_type = 'redcap'
+inner join subject_alias sa2
+    on sa2.subject_id = sa1.subject_id
+    and sa2.project_id = 696 -- not necessary, just to be explicit
+    and sa2.id_type = 'nimh_guid'
+left join rcap_scheduling_form sched_main 
+    on sched_main.source_subject_id = phqa.source_subject_id 
+left join rcap_demographics dem
+    on dem.source_subject_id = phqa.source_subject_id
+left join rcap_pfh_child pfhc 
+    on pfhc.source_subject_id = phqa.source_subject_id
+order by sa2.subject_id;
 
+
+
+
+/* 
+!!! Pat Scherer's original script below!!!!
+!!! See the updated one above !!!
 */
-
 
 -- Step 2: Create NDA cde_phqa9_view itself
 
@@ -91,8 +124,6 @@ left join subject_alias sa3
     and sa3.id_type = 'redcap'
 left join rcap_pfh_child pfhc 
     on pfhc.source_subject_id = sa3.source_subject_id;
-
-
 
 
 
