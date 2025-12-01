26338f80f98f40337422b72a8748f7b33f6ff267
Pat Scherer <72883517+pscherer-tacc@users.noreply.github.com>
2025-12-01 16:06:27 -0600

    Add files via upload
    Drafted SQL for Ressler's Genetics Lab analysis. Query utilizes the rcap_ctau_sample_info_joined_view_new.sql as it's base and can be adapted to the other two labs via filtering the tube_id. There is one outstanding question on whether to expose the dates of last alcohol and drug use or convert these to age (in months). These were not converted in the NDA drug01, but I think these dates should be converted.

---
+++ lab_views/genetics_lab_view.sql
@@ -0,0 +1,210 @@
+--- Construct a view to generate output from the DW for Dr. Ressler's Genetics Lab and Dr. Champagne’s Epigenomics Lab as per the following rqmts:
+--- 	Pull data (all events) for samples sent to Ressler's lab only (si.si_tube_id ilike '%_2_1'):
+---			DOB
+---			Date of blood collection
+---			Calculated age (in days) at date of blood collection
+---			tube_id
+---			Sex
+---			Smoking status
+---			Maternal education (cfhx_parent[1 or 2]_educ AND cfhx_parent[1 or 2]_sex=“female”
+---			# traumas → sum(each trauma category X how_often)
+---			Worst trauma exposure
+---			Age at first trauma exposure
+---			Primary diagnosis
+---			AUDIT score
+---			Request on 11/25/25 to add information from TLFB, SUI, DEQ and AUDIT 
+---
+--- 	Remove any duplicates, pii/phi, and fields not specifically required by the lab
+
+select si_tube_id,
+       source_subject_id,
+       event_name,
+       sched_ctrn_id,
+       subject_id,
+       project_id,
+       id_type,
+       sched_base_complete_date,
+       dem_ch_dob,
+       age_days_between(dem_ch_dob::date, sched_base_complete_date) as age_days,
+       sex,
+       hp_parent1_relationship,
+       hp_parent1_sex,
+       hp_parent1_educ,
+       --hp_parent2_relationship,
+       hp_parent2_gender,
+       hp_parent2_educ,
+	   tlfb_drink_mo,
+	   tlfb_drink_days,
+	   tlfb_drink_per_day,
+	   tlfb_hv_ep_dr_days,
+	   tlfb_24_max,
+	   tlfb_cig_mo,
+       tlfb_smoke_days,
+	   tlfb_cig_per_day,
+	   tlfb_thc_days,
+       tc_1_1,
+       tc_1_1_how_often,
+       tp_1_1_age_first,
+       -- tc_1_1_worst, Worst selections aggregate into tc_8_1
+       tc_1_2,
+       tc_1_2_how_often,
+       tp_1_2_age_first,
+       -- tc_1_2_worst,
+       tc_1_3,
+       tc_1_3_how_often,
+       tp_1_3_age_first,
+       -- tc_1_3_worst,
+       tc_1_4,
+       tc_1_4_how_often,
+       tp_1_4a_age_first,
+       tp_1_4b_age_first,
+       -- tc_1_4_worst,
+       tc_1_5,
+       tc_1_5_how_often,
+       tp_1_5_age_first,
+       -- tc_1_5_worst,
+       tc_1_6,
+       tc_1_6_how_often,
+       tp_1_6_age_first,
+       -- tc_1_6_worst,
+       tc_2_1,
+       tc_2_1_how_often,
+       tp_2_1_age_first,
+       -- tc_2_1_worst,
+       tc_2_2,
+       tc_2_2_how_often,
+       tp_2_2_age_first,
+       -- tc_2_2_worst,
+       tc_2_3,
+       tc_2_3_how_often,
+       tp_2_3_age_first,
+       -- tc_2_3_worst,
+       tc_2_4,
+       tc_2_4_how_often,
+       tp_2_4_age_first,
+       -- tc_2_4_worst,
+       tc_2_5,
+       tc_2_5_how_often,
+       tp_2_5_age_first,
+       -- tc_2_5_worst,
+       tc_3_1,
+       tc_3_1_how_often,
+       tp_3_1_age_first,
+       -- tc_3_1_worst,
+       tc_3_2,
+       tc_3_2_how_often,
+       tp_3_2_age_first,
+       -- tc_3_2_worst,
+       tc_3_3,
+       tc_3_3_how_often,
+       tp_3_3_age_first,
+       -- tc_3_3_worst,
+       tc_4_1,
+       tc_4_1_how_often,
+       tp_4_1_age_first,
+       -- tc_4_1_worst,
+       tc_4_2,
+       tc_4_2_how_often,
+       tp_4_2_age_first,
+       tc_4_2_worst,
+       tc_4_3,
+       tc_4_3_how_often,
+       tp_4_3_age_first,
+       -- tc_4_3_worst,
+       tc_5,
+       tc_5_how_often,
+       tp_5_1_age_first,
+       tp_5_2_age_first,
+       -- tc_5_worst,
+       tc_6_1,
+       tp_6_1_age_first,
+       tc_6_2,
+       tc_6_2_how_often,
+       tp_6_2_age_first,
+       tc_7,
+       tc_7_how_often,
+       tp_7_1_age_first,
+       -- tc_7_worst,
+       tc_8_1 AS worst,
+       age_years_between(tc_8_2::date, dem_ch_dob::date) AS worst_age_yrs,
+       tc_8_3 AS most_recent,
+       tc_8_3_less_than_1mo, -- "1" indicates that the most recent trauma was less than 1 month prior to this visit 
+       age_years_between(tc_8_4::date, dem_ch_dob::date) AS most_recent_trauma_age_yrs, -- Recalculate this into "most_recent_trauma_age_yrs" and remove dates before sharing publicly
+       mini_primary_dx,
+	   audit_q1_sc,
+	   audit_q2_sc,
+	   audit_q3_sc,
+	   audit_q4_sc,
+	   audit_q5_sc,
+	   audit_q6_sc,
+	   audit_q7_sc,
+	   audit_q8_sc,
+	   audit_q9_sc,
+	   audit_q10_sc,
+       audit_score, 
+	   sui_1,
+	   sui_2,
+	   sui_3,
+	   sui_4, 
+	   sui_5,
+	   sui_6,		--- Unstructured text must be curated or omited before sharing
+	   sui_7,
+	   sui_8,
+	   --- deq_alc_use_dt,   ---should these be converted to deq_age_last_alc? (Not converted in the NDA)
+	   deq_alc_last_amt,
+	   deq_alc_dur,
+	   deq_alc_mem_diff,
+	   deq_alc_blackout,
+	   deq_alc_hungover,
+	   deq_alc_effects,
+	   deq_alc_effects_2,
+	   deq_alc_effects_3,
+	   deq_alc_effects_4,
+	   --- deq_drug_use_dt,   ---should these be converted to deq_age_last_drug? (Not converted in the NDA)
+	   deq_drug_mdma,
+	   deq_drug_heroin,
+	   deq_drug_cocaine, 
+	   deq_drug_crack,
+	   deq_drug_k,
+	   deq_drug_meth,
+	   deq_drug_pain,
+	   deq_drug_stim,
+	   deq_drug_k2,
+	   deq_drug_benzos, 
+	   deq_drug_none,
+	   deq_drugs_dur,
+	   deq_drugs_snort,
+	   deq_drugs_inject,
+	   deq_drugs_smoke,
+	   deq_drugs_oral,
+	   deq_drugs_other,
+	   deq_drugs_mem_diff,
+	   deq_drugs_blackout,
+	   deq_drugs_hungover,
+	   deq_drugs_effects,
+	   deq_drugs_effects_2,
+	   deq_drugs_effects_3
+from rcap_ctau_sample_info_joined_view
+--- where si_tube_id ilike '%_1_1'  -- Samples sent to Dr. Champagne's Epigenomics Lab
+where si_tube_id ilike '%_2_1'	-- Samples sent to Dr. Ressler's Genetics Lab
+--- where si_tube_id ilike '%_3_1'	-- Samples sent to Dr. Beurel's Cytokines Lab	 			
+and (tc_administrator is not null OR tc_interview_date is not null);
+
+
+
+
+
+------ Additional queries
+
+-- A query for validtation of the "age_years_between" function
+select
+    source_subject_id,
+    age_years_between(tc_8_2::date, dem_ch_dob::date) AS worst_age_yrs,
+    tc_8_2::date,
+    dem_ch_dob::date,
+    age_years_between(tc_8_4::date, dem_ch_dob::date) AS most_recent_trauma_age_yrs,
+    tc_8_4::date,
+    dem_ch_dob::date
+from rcap_ctau_sample_info_joined_view
+where si_tube_id ilike '%_1_1'
+and (tc_administrator is not null OR tc_interview_date is not null);
