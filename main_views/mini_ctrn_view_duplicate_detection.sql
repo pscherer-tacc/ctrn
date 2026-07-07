25b7ab42f832d12c00a61a83bfc3fe045026d161
Vlad Krendelev <vkren@vkren-mbp22.local>
2026-07-07 16:49:39 -0500

    new miniss view with duplicate detection

---
+++ main_views/mini_ctrn_view_duplicate_detection.sql
@@ -0,0 +1,180 @@
+--- MINI CTRN View
+--- Name of the view: mini_ctrn_view
+--- This view includes date calculations and fields for cross-checking and curation. Curation fields and associated data must be removed 
+--- prior to sharing outside of the CTRN's IRB-approved community. The export from this query includes incomplete records which are curated as follows:
+--- 	1) Records with NULL interview dates are removed
+---		2) Records with NULL sex are removed
+---		3) Remove duplicate incomplete records where sched_[event_name]_complete not equal "1"(complete) or "8"(sufficiently complete)
+---     4) Report any remaining records for correction if sched_[event_name]_complete not equal "1"(complete) or "8"(sufficiently complete), but they contain data 
+---
+-- Query the data from the view
+select
+    case 
+        when mini_duplicates.source_subject_id is not null then 'YES'
+        else 'NO'
+    end,  
+    mini_view.* 
+from mini_ctrn_view mini_view
+left join (
+    select source_subject_id, event_name
+    from mini_ctrn_view
+    group by source_subject_id, event_name
+    having count(*) > 1
+) mini_duplicates
+    on mini_duplicates.source_subject_id = mini_view.source_subject_id
+    and mini_duplicates.event_name = mini_view.event_name
+--where interview_date is not null
+order by 
+    case 
+        when mini_duplicates.source_subject_id is not null then 'YES'
+        else 'NO'
+    end,
+    mini_view.source_subject_id,
+    mini_view.event_name
+;
+
+
+select source_subject_id, event_name
+from mini_ctrn_view
+group by source_subject_id, event_name
+having count(*) > 1;
+
+
+--- The body of the view
+create or replace view mini_ctrn_view
+as
+select
+    dem.dem_guid,			-- only used for NIH projects; may be omitted for CTRN Main studies
+    sa1.subject_id,
+    mini.source_subject_id, -- only for validation; DELETE before sharing
+    mini.event_name,
+    case
+        when mini.event_name like 'baseline%' then to_char(sched_main.sched_base_complete_date, 'mm/dd/yyyy')
+        when mini.event_name like 'one_month%' then to_char(sched_main.sched_1mo_complete_date, 'mm/dd/yyyy')
+        when mini.event_name like 'six_month%' then to_char(sched_main.sched_6mo_complete_date, 'mm/dd/yyyy')
+        when mini.event_name like 'one_year%' then to_char(sched_main.sched_1yr_complete_date, 'mm/dd/yyyy')
+        when mini.event_name like '18_month%' then to_char(sched_main.sched_18mo_complete_date, 'mm/dd/yyyy')
+        when mini.event_name like '24_month%' then to_char(sched_main.sched_2yr_complete_date, 'mm/dd/yyyy')
+	end as interview_date,  -- only for validation; DELETE before sharing
+	case 
+		when mini.event_name like 'baseline%' then nda_months_between(sched_main.sched_base_complete_date, dem.dem_ch_dob)
+		when mini.event_name like 'one_month%' then nda_months_between(sched_main.sched_1mo_complete_date, dem.dem_ch_dob)
+		when mini.event_name like 'six_month%' then nda_months_between(sched_main.sched_6mo_complete_date, dem.dem_ch_dob)
+		when mini.event_name like 'one_year%' then nda_months_between(sched_main.sched_1yr_complete_date, dem.dem_ch_dob)
+		when mini.event_name like '18_month%' then nda_months_between(sched_main.sched_18mo_complete_date, dem.dem_ch_dob)
+		when mini.event_name like '24_month%' then nda_months_between(sched_main.sched_2yr_complete_date, dem.dem_ch_dob)
+	end as interview_age,
+	case
+		when mini.event_name like 'baseline%' then sched_main.sched_base_complete
+		when mini.event_name like 'one_month%' then sched_main.sched_1mo_complete
+		when mini.event_name like 'six_month%' then sched_main.sched_6mo_complete
+		when mini.event_name like 'one_year%' then sched_main.sched_1yr_complete
+		when mini.event_name like '18_month%' then sched_main.sched_18mo_complete
+		when mini.event_name like '24_month%' then sched_main.sched_2yr_complete
+	end as complete, -- only for validation; DELETE before sharing
+    case 
+        when pfhc.hc_sex_birth_cert='1' then 'F'
+        when pfhc.hc_sex_birth_cert='2' then 'M'
+        else null
+    end as sex,
+    case 
+        when pfhc.hc_race='1' then 'American Indian or Alaska Native'
+        when pfhc.hc_race='2' then 'Asian'
+  	    when pfhc.hc_race='3' then 'Native Hawaian or Pacific Islander'
+	    when pfhc.hc_race='4' then 'Black or African American'
+  	    when pfhc.hc_race='5' then 'White'
+  	    when pfhc.hc_race='6' then 'Multi-racial'
+        else null
+    end as race,
+    pfhc.hc_hispanic,
+    case															-- Renaming events as visits to facilitate sequencing as per 4/30/2026 request by Jeff
+	    when mini.event_name like 'baseline%' then '00_baseline'      
+		when mini.event_name like 'one_month%' then '01_one_month'
+		when mini.event_name like 'six_month%' then '06_six_month'
+		when mini.event_name like 'one_year%' then '12_month'
+		when mini.event_name like '18_month%' then '18_month'
+		when mini.event_name like '24_month%' then '24_month'
+	end as visit,
+    mini.mini_a1__1_current,
+    mini.mini_a1__2_past,		
+	mini.mini_a1__3_recurrent,
+	mini.mini_a2__1_current,
+    mini.mini_a2__2_past,
+	mini.mini_a2__3_recurrent,
+	mini.mini_b1__1_current,
+    mini.mini_b1__2_lifetimeatt,		
+    mini.mini_b18,
+	mini.mini_b1a,
+	mini.mini_b3__1_current,
+	mini.mini_b3__2_early_remission,
+	mini.mini_c1__1_current,
+	mini.mini_c1__2_past,
+	mini.mini_c2__1_current,
+	mini.mini_c2__2_past,
+	mini.mini_c2__3_not_explored,
+	mini.mini_c3__1_current,
+	mini.mini_c3__2_past,
+	mini.mini_c5__1_current,
+	mini.mini_c5__2_past,
+	mini.mini_c7__1_current,
+	mini.mini_c7__2_past,
+	mini.mini_d1__1_current,
+	mini.mini_d1__2_lifetime,
+	mini.mini_e1__1_current,
+	mini.mini_f1__1_current,
+	mini.mini_g1__1_current,
+	mini.mini_h1__1_current,
+	mini.mini_i1__1_current,
+	mini.mini_j1__1_current,
+	mini.mini_k1__1_past_12_mo,
+	mini.mini_l1__1_past_12_mo,
+	mini.mini_m,
+	mini.mini_n,
+	mini.mini_n1,
+	mini.mini_o1__1_past_12_mo,
+	mini.mini_o2_a,
+	mini.mini_o2_b,
+	mini.mini_o2_c,
+	mini.mini_o2_d,
+	mini.mini_o2_e,
+	mini.mini_o2_f,
+	mini.mini_o2_g,
+	mini.mini_o2_h,
+	mini.mini_o2_i,
+	mini.mini_o2_j,
+	mini.mini_o2_k,
+	mini.mini_o2_l,
+	mini.mini_o2_m,
+	mini.mini_o2_n,
+	mini.mini_o2_o,
+	mini.mini_o3,
+	mini.mini_o3_onset,
+	mini.mini_p1__1_past_6_mo,
+	mini.mini_q1__2_lifetime,
+	mini.mini_q3__1_current,
+	mini.mini_q3__2_lifetime,
+	mini.mini_q5__1_current,
+	mini.mini_q5__2_lifetime,
+	mini.mini_r1__1_current,
+	mini.mini_s1__1_current,
+	mini.mini_t1__1_current,
+	mini.mini_u1__1_current,
+	mini.mini_v1__1_current,
+	mini.mini_w1,
+	mini.mini_x1__1_not_ruled_out,
+	mini.mini_primary_dx,
+	mini.mini_lang
+from rcap_miniss_v2 mini
+inner join subject_alias sa1
+    on sa1.source_subject_id = mini.source_subject_id
+    and sa1.project_id = 696
+    and sa1.id_type = 'redcap'
+	and mini.event_name not like 'unscheduled%'
+left join rcap_scheduling_form sched_main 
+    on sched_main.source_subject_id = mini.source_subject_id 
+left join rcap_demographics dem
+    on dem.source_subject_id = mini.source_subject_id
+left join rcap_pfh_child pfhc 
+    on pfhc.source_subject_id = mini.source_subject_id
+	and pfhc.event_name like 'baseline%'
+order by sa1.subject_id;
